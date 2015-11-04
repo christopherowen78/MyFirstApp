167535265ef880518fa6435a0397051e2b6952ef
Christopher Owen <christopher.owen@bbc.co.uk>
2015-11-04 18:45:00 +0000

    Resolved errors when running the app
    Had an error when clicking Send button.
    In DisplayMessageActivity.java, I replaced
    R.layout.fragment_display_message with R.layout.activity_display_message
    This fixed the error.

--- app/src/main/java/com/apps/owenc47/myfirstapp/DisplayMessageActivity.java
+++ app/src/main/java/com/apps/owenc47/myfirstapp/DisplayMessageActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.app.Fragment;
 
@@ -16,10 +19,10 @@ public class DisplayMessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
 
-//        if (savedInstanceState == null){
-//            getSupportFragmentManager().beginTransaction()
-//                    .add(R.id.container, new PlaceholderFragment()).commit();
-//        }
+/*        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment()).commit();
+        }*/
 
         // Get the message from the intent
         Intent intent = getIntent();
@@ -41,7 +44,32 @@ public class DisplayMessageActivity extends AppCompatActivity {
         //                .setAction("Action", null).show();
         //    }
         //});
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle app bar item clicks here. The app bar
+        // automatically handles clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        public PlaceholderFragment() {}
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.activity_display_message, container, false);
+            return rootView;
+        }
     }
 
 }
